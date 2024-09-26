127acb7f27d5d5c7f9ddc76964a5a01773305004
Rózsa Patrik <rozsapatrik007@gmail.com>
2024-09-26 09:43:37 +0200

    Guards updated with checks from auth service

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+  
+  constructor(private authService: AuthenticationService, private router: Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    
+      return this.authService.isAdmin();
   }
   
 }
