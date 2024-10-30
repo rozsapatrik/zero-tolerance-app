938a4062bd919c19de8dfda501d4ee96bb9cdacd
Rózsa Patrik <rozsapatrik007@gmail.com>
2024-10-30 09:10:08 +0100

    drinks can be listed out

--- src/app/components/drink-list/drink-list.component.ts
+++ src/app/components/drink-list/drink-list.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { map, Observable } from 'rxjs';
+
+export interface Drink{
+  brand: string;
+  name: string;
+  abv: number;
+  caloriesPerServing: number;
+  category: string;
+  ml: number;
+}
 
 @Component({
   selector: 'app-drink-list',
   templateUrl: './drink-list.component.html',
   styleUrls: ['./drink-list.component.css']
 })
+
 export class DrinkListComponent {
+
+  drinksList: Drink[] = [];
+  
+  constructor(private afs: AngularFirestore){}
+
+  ngOnInit(){
+    this.getDrinks().subscribe(drinks => {this.drinksList = drinks});
+  }
+  
+  getDrinks(): Observable<Drink[]>{
+    return this.afs.collection('drink').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Drink;
+        return data;
+      }))
+    );
+  }
 
 }
