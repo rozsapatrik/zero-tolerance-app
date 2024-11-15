76c7f11a1e1dd79634a121c6e6018c3b6b251af6
Rózsa Patrik <rozsapatrik007@gmail.com>
2024-11-15 22:01:09 +0100

    admin can add new drinks now

--- src/app/components/admin-form/admin-form.component.ts
+++ src/app/components/admin-form/admin-form.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-form',
@@ -10,7 +12,7 @@ export class AdminFormComponent {
 
   drinkForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private afs: AngularFirestore, private router: Router) {}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -20,15 +22,26 @@ export class AdminFormComponent {
     this.drinkForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       abv: [null, [Validators.required, Validators.min(0), Validators.max(100)]],
-      calories: [null, [Validators.required, Validators.min(0)]],
-      volume: [null, [Validators.required, Validators.min(1)]],
+      caloriesPerServing: [null, [Validators.required, Validators.min(0)]],
+      ml: [null, [Validators.required, Validators.min(1)]],
       category: ['', Validators.required]
     });
   }
 
-  onSubmit(): void {
+  async onSubmit(): Promise<void> {
     if (this.drinkForm.valid) {
-      console.log('Form Submitted:', this.drinkForm.value);
+      const drinkData = this.drinkForm.value;
+
+      try {
+        // Add new drink to the Firestore 'drink' collection
+        await this.afs.collection('drink').add(drinkData);
+        console.log('Drink added successfully:', drinkData);
+
+        // Navigate back to the admin page
+        this.router.navigate(['/admin']);
+      } catch (error) {
+        console.error('Error adding drink to Firestore:', error);
+      }
     }
   }
 
