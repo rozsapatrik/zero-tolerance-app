9f557a87788c28374971c257e40ce369fe4572a2
Rózsa Patrik <rozsapatrik007@gmail.com>
2024-11-15 21:29:54 +0100

    listing out drinks on admin page works

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-admin-page',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent {
+
+  drinks: any[] = [];
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit(){
+    this.fetchAllDrinks();
+  }
+
+  fetchAllDrinks(): void {
+    this.afs.collection("drink").valueChanges({idField: 'id'}).subscribe(
+      (drinks) => {
+        this.drinks = drinks;
+        console.log("Fetched drinks: ", this.drinks);
+      },
+      (error) => {
+        console.error('Error fetching drinks', error);
+      }
+    )
+  }
 
 }
