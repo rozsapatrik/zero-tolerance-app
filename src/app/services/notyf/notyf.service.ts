9dbd906946ee904ee23f2d1ec4b6f5188f704858
rozsapatrik <rozsapatrik007@gmail.com>
2025-01-19 17:28:14 +0100

    notyf service added

---
+++ src/app/services/notyf/notyf.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Notyf } from 'notyf';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotyfService {
+  constructor() { }
+  
+  private notyf = new Notyf({
+    duration: 4000, // Duration in milliseconds
+    position: {
+      x: 'right',
+      y: 'top',
+    },
+    types: [
+      {
+        type: 'success',
+        background: '#4CAF50',
+        icon: false,
+      },
+      {
+        type: 'error',
+        background: '#F44336',
+        icon: false,
+      },
+    ],
+  });
+
+  success(message: string): void {
+    this.notyf.success(message);
+  }
+
+  error(message: string): void {
+    this.notyf.error(message);
+  }
+}
