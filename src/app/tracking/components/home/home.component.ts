3ce287a51135a800973d7565b9bfb83ab02c27dc
rozsapatrik <rozsapatrik007@gmail.com>
2025-05-20 20:57:27 +0200

    Add home page animation

--- src/app/tracking/components/home/home.component.ts
+++ src/app/tracking/components/home/home.component.ts
@@ -50,6 +50,21 @@ interface UserData {
         ),
       ]),
     ]),
+    trigger('slide', [
+      transition(':enter', [
+        style({ transform: 'translateY(-20px)', opacity: 0 }),
+        animate(
+          '300ms ease-out',
+          style({ transform: 'translateY(0)', opacity: 1 })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '300ms ease-in',
+          style({ transform: 'translateY(-20px)', opacity: 0 })
+        ),
+      ]),
+    ]),
   ],
 })
 export class HomeComponent implements OnInit {
@@ -321,9 +336,6 @@ export class HomeComponent implements OnInit {
       nextDay.setDate(originalDate.getDate() + 1);
       this.dateService.setSelectedDate(nextDay);
     }
-
-    this.dateService.setSelectedDate(this.selectedDate);
-    this.fetchDrinksForTheDay();
   }
 
   /**
